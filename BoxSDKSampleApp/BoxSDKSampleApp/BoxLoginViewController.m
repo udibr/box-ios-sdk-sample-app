368ac9e4973f2a0f124c1cb4d8f0e2113712bc04
Ryan Lopopolo <lopopolo@box.com>
2013-05-13 18:51:25 -0700

    Do not import <BoxSDK/BoxSDK.h> in the precompiled header
    Importing the SDK in the precompiled header means making changes to the SDK
    requires a clean and a clean build folder. The prefered method to import the
    SDK is to do so in the .m or .h where it is required.

--- BoxSDKSampleApp/BoxSDKSampleApp/BoxLoginViewController.m
+++ BoxSDKSampleApp/BoxSDKSampleApp/BoxLoginViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Box. All rights reserved.
 //
 
+#import <BoxSDK/BoxSDK.h>
+
 #import "BoxFolderViewController.h"
 #import "BoxLoginViewController.h"
 
--- BoxSDKSampleApp/BoxSDKSampleApp/BoxPreviewViewController.m
+++ BoxSDKSampleApp/BoxSDKSampleApp/BoxPreviewViewController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Box. All rights reserved.
 //
 
+#import <BoxSDK/BoxSDK.h>
+
 #import "BoxPreviewViewController.h"
 
 @interface BoxPreviewViewController ()
