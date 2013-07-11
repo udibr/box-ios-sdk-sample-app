ea1bcffa85f4e1a5dbe298bb5ac0779e974c1035
Ehud Ben-Reuven <udi@benreuven.com>
2013-07-11 15:51:02 -0400

    use refresh token

--- FolderPicker-SampleApp/FolderPicker-SampleApp/AppDelegate.m
+++ FolderPicker-SampleApp/FolderPicker-SampleApp/AppDelegate.m
@@ -10,10 +10,46 @@
 #import <BoxSDK/BoxSDK.h>
 #import "ViewController.h"
 
+#import "KeychainItemWrapper.h"
+
+#define REFRESH_TOKEN_KEY   (@"box_api_refresh_token")
+
+@interface AppDelegate ()
+
+@property (nonatomic, readwrite, strong) KeychainItemWrapper *keychain;
+- (void)boxAPITokensDidRefresh:(NSNotification *)notification;
+
+@end
+
 @implementation AppDelegate
+
+@synthesize keychain = _keychain;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    // Setup BoxSDK
+#error Set your client ID and client secret in the BoxSDK
+    [BoxSDK sharedSDK].OAuth2Session.clientID = @"YOUR_CLIENT_ID";
+    [BoxSDK sharedSDK].OAuth2Session.clientSecret = @"YOUR_CLIENT_SECRET";
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(boxAPITokensDidRefresh:)
+                                                 name:BoxOAuth2SessionDidBecomeAuthenticatedNotification
+                                               object:[BoxSDK sharedSDK].OAuth2Session];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(boxAPITokensDidRefresh:)
+                                                 name:BoxOAuth2SessionDidRefreshTokensNotification
+                                               object:[BoxSDK sharedSDK].OAuth2Session];
+    
+    // set up stored OAuth2 refresh token
+    self.keychain = [[KeychainItemWrapper alloc] initWithIdentifier:REFRESH_TOKEN_KEY accessGroup:nil];
+    
+    id storedRefreshToken = [self.keychain objectForKey:(__bridge id)kSecValueData];
+    if (storedRefreshToken)
+    {
+        [BoxSDK sharedSDK].OAuth2Session.refreshToken = storedRefreshToken;
+    }
+
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     // Override point for customization after application launch.
@@ -25,6 +61,18 @@
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (void)boxAPITokensDidRefresh:(NSNotification *)notification
+{
+    BoxOAuth2Session *OAuth2Session = (BoxOAuth2Session *) notification.object;
+    [self setRefreshTokenInKeychain:OAuth2Session.refreshToken];
+}
+
+- (void)setRefreshTokenInKeychain:(NSString *)refreshToken
+{
+    [self.keychain setObject:@"FolderPicker-SampleApp" forKey: (__bridge id)kSecAttrService];
+    [self.keychain setObject:refreshToken forKey:(__bridge id)kSecValueData];
 }
 
 @end
--- FolderPicker-SampleApp/FolderPicker-SampleApp/ViewController.m
+++ FolderPicker-SampleApp/FolderPicker-SampleApp/ViewController.m
@@ -19,6 +19,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -32,29 +33,54 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (void)boxFolderPicker
+{
+    dispatch_sync(dispatch_get_main_queue(), ^(void){
+        
+        NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+        NSString *basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
+        NSString *thumbnailPath = [basePath stringByAppendingPathComponent:@"BOX"];
+        
+        self.folderPicker = [[BoxSDK sharedSDK] folderPickerWithRootFolderID:BoxAPIFolderIDRoot
+                                                           thumbnailsEnabled:YES
+                                                        cachedThumbnailsPath:thumbnailPath
+                                                        fileSelectionEnabled:YES];
+        self.folderPicker.delegate = self;
+        
+        UINavigationController *controller = [[BoxFolderPickerNavigationController alloc] initWithRootViewController:self.folderPicker];
+        controller.modalPresentationStyle = UIModalPresentationFormSheet;
+        
+        [self presentViewController:controller animated:YES completion:nil];
+    });
+}
+
+- (void)boxError:(NSError*)error
+{
+    if (error.code == BoxSDKOAuth2ErrorAccessTokenExpiredOperationReachedMaxReenqueueLimit) {
+        [self boxFolderPicker];
+        return;
+    }
+    if (error.code == BoxSDKOAuth2ErrorAccessTokenExpired) {
+        return;
+    }
+    dispatch_sync(dispatch_get_main_queue(), ^(void){
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Box" message:[error localizedDescription] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [alert show];
+    });
+}
+
 - (IBAction)browseAction:(id)sender {
+    [[BoxSDK sharedSDK].foldersManager folderInfoWithID:@"0" requestBuilder:nil
+                                                success:^(BoxFolder * folder) {
+                                                    [self boxFolderPicker];
+                                                }
+                                                failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, NSDictionary *JSONDictionary){
+                                                    [self boxError:error];
+                                                }];
     
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *basePath = ([paths count] > 0) ? [paths objectAtIndex:0] : nil;
-    NSString *thumbnailPath = [basePath stringByAppendingPathComponent:@"BOX"];
-    
-#error Set your client ID and client secret in the BoxSDK
-    [BoxSDK sharedSDK].OAuth2Session.clientID = @"YOUR_CLIENT_ID";
-    [BoxSDK sharedSDK].OAuth2Session.clientSecret = @"YOUR_CLIENT_SECRET";
-    
-    self.folderPicker = [[BoxSDK sharedSDK] folderPickerWithRootFolderID:BoxAPIFolderIDRoot
-                                                       thumbnailsEnabled:YES
-                                                    cachedThumbnailsPath:thumbnailPath
-                                                    fileSelectionEnabled:YES];
-    self.folderPicker.delegate = self;
-    
-    UINavigationController *controller = [[BoxFolderPickerNavigationController alloc] initWithRootViewController:self.folderPicker];
-    controller.modalPresentationStyle = UIModalPresentationFormSheet;
+}
 
-    [self presentViewController:controller animated:YES completion:nil];
-}   
-
-- (IBAction)purgeAction:(id)sender 
+- (IBAction)purgeAction:(id)sender
 {
     [self.folderPicker purgeCache];
 }
